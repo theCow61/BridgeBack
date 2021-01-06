6cbc8452311af9f1fd256cdcc0063094e8405a73
theCow61 <theCow61@github.com>
2021-01-05 19:58:21 -0600

    doesn't crash hopefully now

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ Should the telegram chat be its own chat?
 using System;
 using Discord;
 using Telegram;
+using System.IO;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using Telegram.Bot;
@@ -20,21 +21,19 @@ namespace BridgeBack
         
         private DiscordSocketClient _client;
         private ITelegramBotClient botClient;
-        SocketMessage broh;
         async Task MainAsync()
         {
             _client = new DiscordSocketClient();
             _client.MessageReceived += CommandHandler;
             _client.Log += Log;
             string DiscordToken = "Nzk0NzkyNTU4MDcyODIzODI4.X-_-QA.NyOtg7qs138274WagyVKspSLy38";
-            string TelegramToken = "1255929985:AAFZVSUX-nxjTXGb15RFA-mXt0GiPam7sWs";
+            string TelegramToken = "1540037178:AAFpOZap7GNOByt2RSk69YSG9gDRGRA6CSA";
             botClient = new TelegramBotClient(TelegramToken);
             var me = botClient.GetMeAsync().Result;
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
             await _client.LoginAsync(TokenType.Bot, DiscordToken);
             await _client.StartAsync();
-
             // Block task until program closed
             await Task.Delay(-1);
         }
@@ -50,27 +49,44 @@ namespace BridgeBack
             if (message.Channel.ToString().Equals("bridge-to-telegram") == false)
                 return Task.CompletedTask;
             
-            //Console.WriteLine(message.Channel.ToString());
-            //Console.WriteLine(message.Content.ToString());
             string mesg = message.Content.ToString();
             botClient.SendTextMessageAsync(chatId: "-1001345308114", text: $@"{message.Author.ToString()} says: {mesg}", disableNotification: true);
-            
-            broh = message;
             return Task.CompletedTask;
         }
         private async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
-            if (e.Message.Text.Contains("#bridge") && e.Message.From.IsBot == false)
+            if (e.Message.Text != null)
             {
-                try
+                if (e.Message.Text.Contains("#bridge") && e.Message.From.IsBot == false)
                 {
-                    string refinedMessage = e.Message.Text.Replace("#bridge", "");
-                    await broh.Channel.SendMessageAsync($@"{e.Message.From.FirstName} says: {refinedMessage}");
-                } catch (Exception easports)
-                {
-                    Console.WriteLine(easports);
+                    try
+                    {
+                        string refinedMessage = e.Message.Text.Replace("#bridge", "");
+                        SocketChannel channel = _client.GetChannel(794799398734266388);
+                        await (channel as IMessageChannel).SendMessageAsync($@"{e.Message.From.FirstName} says: {refinedMessage}");
+                    } catch (Exception easports)
+                    {
+                        Console.WriteLine(easports);
+                    }
                 }
             }
+            /*if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document) // figure this out
+            {
+                if (e.Message.Caption != null && e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+                {
+                    if (e.Message.Caption.Contains("#bridge") && e.Message.From.IsBot == false)
+                    {
+                        MemoryStream ms = new MemoryStream();
+                        SocketChannel channel = _client.GetChannel(794799398734266388);
+                        var iSend = channel as IMessageChannel;
+
+                        var file = await botClient.GetFileAsync(e.Message.Document.FileId);
+                        await botClient.DownloadFileAsync(file.FilePath, destination: ms);
+                        await iSend.SendFileAsync(stream: ms, filename: e.Message.Document.FileName);
+                    
+                    }
+                }
+            }*/
         }
     }
 }
